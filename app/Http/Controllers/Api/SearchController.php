7b3d5162de68cf4e19feae7f2f5fff9f934e03d1
an.nguyenhoai <an.nguyenhoai@tiki.vn>
2017-09-13 12:09:17 +0700

    first commit

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SearchController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('home');
+    }
+    public function search(Request $request){
+        // First we define the error message we are going to show if no keywords
+        // existed or if no results found.
+        $error = ['error' => 'No results found, please try with different keywords.'];
+
+        // Making sure the user entered a keyword.
+        if($request->has('q')) {
+
+            // Using the Laravel Scout syntax to search the products table.
+            $posts = Product::search($request->get('q'))->get();
+
+            // If there are results return them, if none, return the error message.
+            return $posts->count() ? $posts : $error;
+
+        }
+
+        // Return the error message if no keywords existed
+        return $error;
+    }
+}
